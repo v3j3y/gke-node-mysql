742d7d2931e3209f10c937640d03fda3b588dd9a
Vejey Gandier <>
2018-12-20 13:16:15 -0500

    first commit

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 'use strict';
 
 const express = require('express');
+const mysql  = require('mysql');
 
 // Constants
 const PORT = 3000;
 
+
 // App
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world\n');
+
+  //Connect to DB
+  const mysqlCon = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+  });
+
+  mysqlCon.connect((err) => {
+    if(err) {
+      return res.json({
+        confirmation: 'fail',
+        message: err
+      })
+    }
+    return res.json({
+      confirmation: 'success',
+    })
+  })
+
 });
 
 app.listen(PORT);
